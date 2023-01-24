1208903a7f5f10a2e9957f6c9b0549bf46e42a8b
alexpurbo <purboalexander@gmail.com>
2023-01-24 21:59:17 +0700

    add dark mode dan perbaikan fitur

--- js/script.js
+++ js/script.js
@@ -2,11 +2,16 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const scrollTop = document.querySelector("#scroll-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    scrollTop.classList.remove("hidden");
+    scrollTop.classList.add("flex");
   } else {
     header.classList.remove("navbar-fixed");
+    scrollTop.classList.add("hidden");
+    scrollTop.classList.remove("flex");
   }
 };
 
@@ -18,3 +23,32 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// Close Navbar without click hamburger button
+window.addEventListener("click", function (e) {
+  if (e.target != navMenu && e.target != hamburger) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// Drk Mode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// Pindahkan posisi toggle dark sesuai mode local storage mode
+if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
